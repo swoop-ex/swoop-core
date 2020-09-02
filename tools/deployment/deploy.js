2ea13f9f2de186dda8dab16e04940aa8551763cb
Dennis Won <jhwon0820@gmail.com>
2020-09-02 12:07:25 -0700

    script updates

--- tools/deployment/deploy.js
+++ tools/deployment/deploy.js
@@ -16,11 +16,15 @@ const Network = require('../network.js');
 const { getAddress } = require('@harmony-js/crypto');
 
 const contracts = [
-  // 'Migrations', 'SafeMath',
-  // 'IUniswapV2Callee', 'IUniswapV2HRC20', 'IHRC20', 'IUniswapV2Factory', 'IUniswapV2Pair',
-  // 'Math', 'Multicall', 'UQ112x112', 'UniswapV2HRC20', 'WONE',
-  // 'UniswapV2Pair',
-  'UniswapV2Factory', 'HRC20'
+  'Migrations',
+  'SafeMath',
+  'Math',
+  'Multicall',
+  'UQ112x112',
+  'UniswapV2HRC20',
+  'UniswapV2Pair',
+  'UniswapV2Factory',
+  'WONE',
 ];
 
 // Vars
@@ -29,9 +33,9 @@ network.hmy.wallet.addByPrivateKey(network.privateKeys.deployer);
 
 async function deploy() {
   for (let i = 0; i < contracts.length; i++) {
-    const contract = contracts[i];
-    const addr = await deployContract(contract);
-    console.log(`    Deployed contract ${contract}: ${addr} (${getAddress(addr).bech32})`);
+    const contract = contracts[i]
+    const addr = await deployContract(contract)
+    console.log(`    Deployed contract ${contract}: ${addr} (${getAddress(addr).bech32})`)
   }
 }
 
